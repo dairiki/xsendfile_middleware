fea45050a016b3b75467f1f4689e852e572d7678
Jeff Dairiki <dairiki@dairiki.org>
2015-05-08 14:33:07 -0700

    Back to dev

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1a3'
+version = '0.1a4.dev0'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
